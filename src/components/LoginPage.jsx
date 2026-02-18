617a3257842fb649b872765b85940415b0a0b373
Jayesh Malviya <jayeshmalviya10@gmail.com>
2026-02-18 22:31:24 +0530

    fixes

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axiosClient from "../lib/axiosClient";
+import axios from "axios";
 
 const highlights = [
   "Instant booking confirmation",
